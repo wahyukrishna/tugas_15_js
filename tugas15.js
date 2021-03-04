03af9da117f14377c253bc858be4763a55a9f157
Wahyu Krishna <hanzo180895@gmail.com>
2021-03-04 22:02:54 +0700

    Tugas 15 JS
    Tugas Niomic

---
+++ tugas15.js
@@ -0,0 +1,15 @@
+//sisi AB
+var x = 8;
+var AB = Math.pow(x,2);
+console.log("Nilai AB : "+x);
+
+//sisi BC
+var y = 6;
+var BC = Math.pow(y,2);
+console.log("Nilai BC : "+y);
+
+//mencari sisi miring AC
+var AC = AB + BC;
+var hasil = Math.sqrt(AC);
+console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah : "+hasil+" cm");
+
